ae7657b4a1147a7c04f1388f6ac87b4561727bfc
Clément Gamé <cg@alphalyr.com>
2018-05-08 12:24:56 +0200

    First Commit.

---
+++ ARIMASharp/ARIMA.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MathNet.Numerics.IntegralTransforms;
+
+namespace ARIMASharp
+{
+    public class ARIMA
+    {
+
+        ARIMA(int ar_order, int ma_order )
+        {
+
+        }
+
+        void Predict()
+        {
+
+        }
+
+    }
+}
